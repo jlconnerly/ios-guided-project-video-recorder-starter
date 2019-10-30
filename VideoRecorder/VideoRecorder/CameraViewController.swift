4666beb845c2d93970c8de8e937d444c89dc022e
Paul Solt <PaulSolt@gmail.com>
2019-10-30 12:57:57 -0400

    Added camera authorization Started to implement logic to pick camera to display on live preview

--- VideoRecorder/VideoRecorder/CameraViewController.swift
+++ VideoRecorder/VideoRecorder/CameraViewController.swift
@@ -7,16 +7,57 @@
 //
 
 import UIKit
+import AVFoundation
 
 class CameraViewController: UIViewController {
 
+	// AVCaptureSession
+	lazy private var captureSession = AVCaptureSession()
+	
     @IBOutlet var recordButton: UIButton!
     @IBOutlet var cameraView: CameraPreviewView!
 
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+		
+		setUpSession()
+	}
+	
+	private func setUpSession() {
+		
+		captureSession.beginConfiguration()
+		
+		// Add the camera input
+		let camera = bestCamera()
+		
+		guard let cameraInput = try? AVCaptureDeviceInput(device: camera) else {
+			fatalError("Cannot create a device input from camera")
+		}
+		
+		guard captureSession.canAddInput(cameraInput) else {
+			fatalError("Cannot add camera to capture session")
+		}
+		captureSession.addInput(cameraInput)
+		
+		
+		// Set video mode
+		
+		// Add the audio input
+		
+		
+		captureSession.commitConfiguration()
+		
+		// Start the session and show the live preview
+	}
+	
+	private func bestCamera() -> AVCaptureDevice {
+		if let device = AVCaptureDevice.default(.builtInUltraWideCamera, for: .video, position: .back) {
+			return device
+		} else if let device = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back) {
+			return device
+		}
+		fatalError("ERROR: No cameras on the device or you are running on the Simulator")
 	}
 
 
